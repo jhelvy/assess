66e66056f6759e3b68c4962167f85f599c473db6
John Helveston <john.helveston@gmail.com>
2022-12-17 20:51:52 -0500

    added grading scales to utils

--- R/util.R
+++ R/util.R
@@ -1,4 +1,35 @@
 
+#' Get an assessment scale
+#'
+#' @param n Scale size
+#' @export
+get_scale <- function(n) {
+    if (n == 3) {
+        return(data.frame(
+            assessment = seq(0, 3),
+            score = c(0, 0.5, 0.8, 1)
+        ))
+    }
+    if (n == 4) {
+        return(data.frame(
+            assessment = seq(0, 4),
+            score = c(0, 0.5, 0.75, 0.9, 1)
+        ))
+    }
+    if (n == 5) {
+        return(data.frame(
+            assessment = seq(0, 5),
+            score = c(0, 0.5, 0.7, 0.8, 0.9, 1)
+        ))
+    }
+    if (n == 6) {
+        return(data.frame(
+            assessment = seq(0, 6),
+            score = c(0, 0.5, 0.7, 0.75, 0.8, 0.9, 1)
+        ))
+    }
+}
+
 #' Makes a directory
 #'
 #' Makes a directory
